7df0d146242bf609e44001479b31dcde8e0cc5a3
焕 <>
2025-04-20 18:07:38 +0800

    test

---
+++ examples/browser/main.py
@@ -0,0 +1,58 @@
+import logging
+import os
+import ssl
+
+from langchain_openai import ChatOpenAI
+from browser_use import Agent, Browser
+from browser_use import BrowserConfig
+from httpx import Client, AsyncClient
+
+import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# 配置日志
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format='%(asctime)s - %(levelname)s - %(message)s',
+#     datefmt='%Y-%m-%d %H:%M:%S'
+# )
+# logger = logging.getLogger(__name__)
+print(os.getenv("OPENAI_API_KEY"))  # Should print your key
+# 禁用 SSL 验证（不推荐用于生产环境）
+os.environ['REQUESTS_CA_BUNDLE'] = ""
+ssl._create_default_https_context = ssl._create_unverified_context
+llm = ChatOpenAI(
+    base_url="https://api.siliconflow.cn/v1/",
+    model="Qwen/Qwen2.5-72B-Instruct-128K",
+    api_key="sk-jgzpphkqtbjpdwkuqiaclvuussojnchgrdarirnwubmlfpkl",
+    # base_url="https://api.hunyuan.cloud.tencent.com/v1/",
+    # model="hunyuan-vision",
+    # api_key="sk-VMGNgGpim4ztGDiQtx9Gzm6S92PcJeaabchVrqcmc2DLIb59",
+    # base_url="https://ide.tools.cmic.site/app/tiangong/openai-service/8080/v1/",
+    # model="deepseek-v3",
+    # api_key="sk-lrhdwxwgnkrmqviiuzoqifucbpdddevpnucrvfehabiaxxmj",
+    http_client=Client(verify=False, proxy="http://localhost:8888"),
+    http_async_client=AsyncClient(verify=False, proxy="http://localhost:8888"),
+)
+
+async def main():   
+    # logger.info("初始化Agent和LLM...")
+    config = BrowserConfig(
+        # chrome_instance_path="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+        cdp_url="http://localhost:9222"
+    )
+    browser = Browser(config=config)                 
+    agent = Agent(
+        task="打开网址www.baidu.com",
+        llm=llm,
+        browser=browser
+
+    )
+    # logger.info("开始执行任务")
+    await agent.run()
+    # logger.info("任务执行完成")
+
+if __name__ == "__main__":
+    asyncio.run(main())
